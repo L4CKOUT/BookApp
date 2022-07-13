edb7f3ff91817f93f45916753e49115a4ffc0a17
Timothy Tan <timothytan967>
2022-07-13 13:05:58 +0800

    entity test

---
+++ WebApplication/src/test/java/Booker/WebApplication/Model/AuthorTest.java
@@ -0,0 +1,213 @@
+package Booker.WebApplication.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+class AuthorTest {
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>{@link Author#Author()}
+     *   <li>{@link Author#setBooks(Set)}
+     *   <li>{@link Author#setFirstName(String)}
+     *   <li>{@link Author#setId(Long)}
+     *   <li>{@link Author#setLastName(String)}
+     *   <li>{@link Author#toString()}
+     *   <li>{@link Author#getBooks()}
+     *   <li>{@link Author#getFirstName()}
+     *   <li>{@link Author#getId()}
+     *   <li>{@link Author#getLastName()}
+     * </ul>
+     */
+    @Test
+    void testConstructor() {
+        Author actualAuthor = new Author();
+        HashSet<Book> bookSet = new HashSet<>();
+        actualAuthor.setBooks(bookSet);
+        actualAuthor.setFirstName("Jane");
+        actualAuthor.setId(123L);
+        actualAuthor.setLastName("Doe");
+        String actualToStringResult = actualAuthor.toString();
+        assertSame(bookSet, actualAuthor.getBooks());
+        assertEquals("Jane", actualAuthor.getFirstName());
+        assertEquals(123L, actualAuthor.getId().longValue());
+        assertEquals("Doe", actualAuthor.getLastName());
+        assertEquals("Author{id=123, firstName='Jane', lastName='Doe'}", actualToStringResult);
+    }
+
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>{@link Author#Author(String, String)}
+     *   <li>{@link Author#setBooks(Set)}
+     *   <li>{@link Author#setFirstName(String)}
+     *   <li>{@link Author#setId(Long)}
+     *   <li>{@link Author#setLastName(String)}
+     *   <li>{@link Author#toString()}
+     *   <li>{@link Author#getBooks()}
+     *   <li>{@link Author#getFirstName()}
+     *   <li>{@link Author#getId()}
+     *   <li>{@link Author#getLastName()}
+     * </ul>
+     */
+    @Test
+    void testConstructor2() {
+        Author actualAuthor = new Author("Jane", "Doe");
+        HashSet<Book> bookSet = new HashSet<>();
+        actualAuthor.setBooks(bookSet);
+        actualAuthor.setFirstName("Jane");
+        actualAuthor.setId(123L);
+        actualAuthor.setLastName("Doe");
+        String actualToStringResult = actualAuthor.toString();
+        assertSame(bookSet, actualAuthor.getBooks());
+        assertEquals("Jane", actualAuthor.getFirstName());
+        assertEquals(123L, actualAuthor.getId().longValue());
+        assertEquals("Doe", actualAuthor.getLastName());
+        assertEquals("Author{id=123, firstName='Jane', lastName='Doe'}", actualToStringResult);
+    }
+
+    /**
+     * Method under test: {@link Author#equals(Object)}
+     */
+    @Test
+    void testEquals() {
+        Author author = new Author();
+        author.setBooks(new HashSet<>());
+        author.setFirstName("Jane");
+        author.setId(123L);
+        author.setLastName("Doe");
+        assertNotEquals(author, null);
+    }
+
+    /**
+     * Method under test: {@link Author#equals(Object)}
+     */
+    @Test
+    void testEquals2() {
+        Author author = new Author();
+        author.setBooks(new HashSet<>());
+        author.setFirstName("Jane");
+        author.setId(123L);
+        author.setLastName("Doe");
+        assertNotEquals(author, "Different type to Author");
+    }
+
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>{@link Author#equals(Object)}
+     *   <li>{@link Author#hashCode()}
+     * </ul>
+     */
+    @Test
+    void testEquals3() {
+        Author author = new Author();
+        author.setBooks(new HashSet<>());
+        author.setFirstName("Jane");
+        author.setId(123L);
+        author.setLastName("Doe");
+        assertEquals(author, author);
+        int expectedHashCodeResult = author.hashCode();
+        assertEquals(expectedHashCodeResult, author.hashCode());
+    }
+
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>{@link Author#equals(Object)}
+     *   <li>{@link Author#hashCode()}
+     * </ul>
+     */
+    @Test
+    void testEquals4() {
+        Author author = new Author();
+        author.setBooks(new HashSet<>());
+        author.setFirstName("Jane");
+        author.setId(123L);
+        author.setLastName("Doe");
+
+        Author author1 = new Author();
+        author1.setBooks(new HashSet<>());
+        author1.setFirstName("Jane");
+        author1.setId(123L);
+        author1.setLastName("Doe");
+        assertEquals(author, author1);
+        int expectedHashCodeResult = author.hashCode();
+        assertEquals(expectedHashCodeResult, author1.hashCode());
+    }
+
+    /**
+     * Method under test: {@link Author#equals(Object)}
+     */
+    @Test
+    void testEquals5() {
+        Author author = new Author();
+        author.setBooks(new HashSet<>());
+        author.setFirstName("Jane");
+        author.setId(1L);
+        author.setLastName("Doe");
+
+        Author author1 = new Author();
+        author1.setBooks(new HashSet<>());
+        author1.setFirstName("Jane");
+        author1.setId(123L);
+        author1.setLastName("Doe");
+        assertNotEquals(author, author1);
+    }
+
+    /**
+     * Method under test: {@link Author#equals(Object)}
+     */
+    @Test
+    void testEquals6() {
+        Author author = new Author();
+        author.setBooks(new HashSet<>());
+        author.setFirstName("Jane");
+        author.setId(null);
+        author.setLastName("Doe");
+
+        Author author1 = new Author();
+        author1.setBooks(new HashSet<>());
+        author1.setFirstName("Jane");
+        author1.setId(123L);
+        author1.setLastName("Doe");
+        assertNotEquals(author, author1);
+    }
+
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>{@link Author#equals(Object)}
+     *   <li>{@link Author#hashCode()}
+     * </ul>
+     */
+    @Test
+    void testEquals7() {
+        Author author = new Author();
+        author.setBooks(new HashSet<>());
+        author.setFirstName("Jane");
+        author.setId(null);
+        author.setLastName("Doe");
+
+        Author author1 = new Author();
+        author1.setBooks(new HashSet<>());
+        author1.setFirstName("Jane");
+        author1.setId(null);
+        author1.setLastName("Doe");
+        assertEquals(author, author1);
+        int expectedHashCodeResult = author.hashCode();
+        assertEquals(expectedHashCodeResult, author1.hashCode());
+    }
+}
+
---
+++ WebApplication/src/test/java/Booker/WebApplication/Model/BookTest.java
@@ -0,0 +1,107 @@
+package Booker.WebApplication.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+
+
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>{@link Book#Book(String, String)}
+     *   <li>{@link Book#setAuthors(Set)}
+     *   <li>{@link Book#setId(Long)}
+     *   <li>{@link Book#setIsbn(String)}
+     *   <li>{@link Book#setPublisher(Publisher)}
+     *   <li>{@link Book#setTitle(String)}
+     *   <li>{@link Book#toString()}
+     *   <li>{@link Book#getAuthors()}
+     *   <li>{@link Book#getId()}
+     *   <li>{@link Book#getIsbn()}
+     *   <li>{@link Book#getPublisher()}
+     *   <li>{@link Book#getTitle()}
+     * </ul>
+     */
+    @Test
+    void testConstructor2() {
+        Book actualBook = new Book("Dr", "Isbn");
+        HashSet<Author> authorSet = new HashSet<>();
+        actualBook.setAuthors(authorSet);
+        actualBook.setId(123L);
+        actualBook.setIsbn("Isbn");
+        Publisher publisher = new Publisher();
+        publisher.setAddressLine("42 Main St");
+        publisher.setBooks(new HashSet<>());
+        publisher.setCity("Oxford");
+        publisher.setId(123L);
+        publisher.setName("Name");
+        publisher.setState("MD");
+        publisher.setZip("21654");
+        actualBook.setPublisher(publisher);
+        actualBook.setTitle("Dr");
+        String actualToStringResult = actualBook.toString();
+        assertSame(authorSet, actualBook.getAuthors());
+        assertEquals(123L, actualBook.getId().longValue());
+        assertEquals("Isbn", actualBook.getIsbn());
+        assertSame(publisher, actualBook.getPublisher());
+        assertEquals("Dr", actualBook.getTitle());
+        assertEquals("Book{id=123, title='Dr', isbn='Isbn'}", actualToStringResult);
+    }
+
+
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>{@link Book#equals(Object)}
+     *   <li>{@link Book#hashCode()}
+     * </ul>
+     */
+    @Test
+    void testEquals() {
+        Publisher publisher = new Publisher();
+        publisher.setAddressLine("42 Main St");
+        publisher.setBooks(new HashSet<>());
+        publisher.setCity("Oxford");
+        publisher.setId(123L);
+        publisher.setName("Name");
+        publisher.setState("MD");
+        publisher.setZip("21654");
+
+        Book book = new Book();
+        book.setAuthors(new HashSet<>());
+        book.setId(123L);
+        book.setIsbn("Isbn");
+        book.setPublisher(publisher);
+        book.setTitle("Dr");
+
+        Publisher publisher1 = new Publisher();
+        publisher1.setAddressLine("42 Main St");
+        publisher1.setBooks(new HashSet<>());
+        publisher1.setCity("Oxford");
+        publisher1.setId(123L);
+        publisher1.setName("Name");
+        publisher1.setState("MD");
+        publisher1.setZip("21654");
+
+        Book book1 = new Book();
+        book1.setAuthors(new HashSet<>());
+        book1.setId(123L);
+        book1.setIsbn("Isbn");
+        book1.setPublisher(publisher1);
+        book1.setTitle("Dr");
+        assertEquals(book, book1);
+        int expectedHashCodeResult = book.hashCode();
+        assertEquals(expectedHashCodeResult, book1.hashCode());
+    }
+
+
+}
+
